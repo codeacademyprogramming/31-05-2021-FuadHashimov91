92feb39483aa267b99d3d5a8cb997b35c7a485ae
FuadHashimov91 <fuadbh@code.edu.az>
2021-05-31 21:06:32 +0400

    AddApi

---
+++ weather/src/data/weatherApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiKey = '16596fe956171a7376f2ba91213e3499';
+
+export const getWeatherData = async (cityname) => {
+    try {
+        const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}`);
+        return data;
+    } 
+    catch (error) {
+        throw error;
+    }
+}
